742ae93120863b48ce9e67a9ac7091a264597a76
Yahya <yeahyes766@gmail.com>
2024-11-28 12:50:03 +0300

    updated colour of image composable

--- app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,17 +49,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 //its used to create user interface
 fun GreetingText(message: String, from: String) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-        modifier = Modifier.padding(8.dp)
-    )
-    Text(
-        text = from,
-        fontSize = 32.sp,
-        modifier = Modifier.padding(8.dp)
-    )
+    Column (verticalArrangement = Arrangement.Center) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            color = Color.Red,
+            lineHeight = 116.sp,
+            modifier = Modifier.padding(8.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            color = Color.Green,
+            fontSize = 32.sp,
+            modifier = Modifier.padding(8.dp).align(alignment = Alignment.End)
+
+        )
+    }
 }
 
 @Composable
